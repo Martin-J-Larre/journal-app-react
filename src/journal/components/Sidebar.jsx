6a0a41604d9b4a39e608eb416fc45e8c782dddfe
Martin-J-Larre <martinjlarre@gmail.com>
2023-05-01 00:11:16 -0400

    #4 Add HomeLayout, Navbar and Sidebar component

---
+++ src/journal/components/Sidebar.jsx
@@ -0,0 +1,57 @@
+import { TurnedInNot } from "@mui/icons-material";
+import {
+  Box,
+  Divider,
+  Drawer,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React from "react";
+
+export const Sidebar = ({ drawerWidth = 240 }) => {
+  return (
+    <Box
+      component="nav"
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    >
+      <Drawer
+        variant="permanent"
+        open
+        sx={{
+          display: { xs: "block" },
+          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Fulano Smith
+          </Typography>
+        </Toolbar>
+        <Divider />
+        <List>
+          {["Enero", "Febrero", "Marzo", "Abril", "Mayo"].map((mes) => (
+            <ListItem key={mes} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <TurnedInNot />
+                </ListItemIcon>
+                <Grid container>
+                  <ListItemText primary={mes} />
+                  <ListItemText
+                    secondary={"Loren aaaa aaaa aaaaa aaaaaa aaaaaa"}
+                  />
+                </Grid>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
--- src/journal/pages/HomePage.jsx
+++ src/journal/pages/HomePage.jsx
@@ -1,5 +1,15 @@
 import { Typography } from "@mui/material";
+import { HomeLayout } from "../layout/HomeLayout";
 
 export const HomePage = () => {
-  return <Typography variant="h1">HomePage</Typography>;
+  return (
+    <HomeLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure autem ab
+        aliquid tempore, facere esse voluptate explicabo sapiente perferendis.
+        Quo nobis tempore fugiat fugit voluptatibus harum, est assumenda
+        dignissimos mollitia.
+      </Typography>
+    </HomeLayout>
+  );
 };
