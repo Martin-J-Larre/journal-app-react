bd3b4cb77a3d08cac6a2a6be9a95a156305c08c4
Martin-J-Larre <martinjlarre@gmail.com>
2023-05-01 18:07:29 -0400

    #5 Add Views components, ImageGallery and floating btn

--- src/journal/pages/HomePage.jsx
+++ src/journal/pages/HomePage.jsx
@@ -1,15 +1,34 @@
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import { HomeLayout } from "../layout/HomeLayout";
+import { NothingSelectedView } from "../views/NothingSelectedView";
+import { NoteView } from "../views/NoteView";
+import { AddOutlined } from "@mui/icons-material";
 
 export const HomePage = () => {
   return (
     <HomeLayout>
-      <Typography>
+      {/* <Typography>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure autem ab
         aliquid tempore, facere esse voluptate explicabo sapiente perferendis.
         Quo nobis tempore fugiat fugit voluptatibus harum, est assumenda
         dignissimos mollitia.
-      </Typography>
+      </Typography> */}
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.8 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 25 }} />
+      </IconButton>
     </HomeLayout>
   );
 };
