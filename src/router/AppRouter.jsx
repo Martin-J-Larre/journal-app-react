4d750d43a41713928f1584e20c38453113da4e98
Martin-J-Larre <martinjlarre@gmail.com>
2023-05-09 23:26:24 -0400

    #12 loging auth with routes added and custom hook useCheckAuth

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,19 +1,24 @@
-import { useSelector } from "react-redux";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import { CheckingAuth } from "../ui/";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { useCheckAuth } from "../hooks";
 
 export const AppRouter = () => {
-  const { status } = useSelector((state) => state.auth);
-  console.log(status);
+  const { status } = useCheckAuth();
+
   if (status === "checking") {
     return <CheckingAuth />;
   }
   return (
     <Routes>
-      <Route path="/auth/*" element={<AuthRoutes />} />
-      <Route path="/*" element={<JournalRoutes />} />
+      {status === "autheticated" ? (
+        <Route path="/*" element={<JournalRoutes />} />
+      ) : (
+        <Route path="/auth/*" element={<AuthRoutes />} />
+      )}
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
     </Routes>
   );
 };
